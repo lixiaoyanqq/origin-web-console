47a360a4a32810548aa0099711c036cfc59d328e
David Taylor <dtaylor113@gmail.com>
2017-07-11 14:16:13 -0400

    Upgrade to angular-patternfly v4.1.1 and patternfly v3.25.1

--- app/scripts/directives/istagSelect.js
+++ app/scripts/directives/istagSelect.js
@@ -53,7 +53,7 @@ angular.module("openshiftConsole")
           $scope.isByNamespace[ns] = {};
           $scope.isNamesByNamespace[ns] = [];
 
-          if (!_.contains($scope.namespaces, ns)) {
+          if (!_.includes($scope.namespaces, ns)) {
             $scope.namespaces.push(ns);
             $scope.isNamesByNamespace[ns] = $scope.isNamesByNamespace[ns].concat($scope.istag.imageStream);
             $scope.isByNamespace[ns][$scope.istag.imageStream] = {status: {tags: [{tag: $scope.istag.tagObject.tag}]}};
@@ -67,7 +67,7 @@ angular.module("openshiftConsole")
             $scope.isNamesByNamespace[ns] = _.keys(imageStreams).sort();
 
             //  Image stream is missing
-            if (!_.contains($scope.isNamesByNamespace[ns], $scope.istag.imageStream)) {
+            if (!_.includes($scope.isNamesByNamespace[ns], $scope.istag.imageStream)) {
               $scope.isNamesByNamespace[ns] = $scope.isNamesByNamespace[ns].concat($scope.istag.imageStream);
               $scope.isByNamespace[ns][$scope.istag.imageStream] = {
                 status: {
