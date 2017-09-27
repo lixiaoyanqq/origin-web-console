2a528fdb5c7aea7cacc1e460d21f7918d6a14e4b
Samuel Padgett <spadgett@redhat.com>
2017-09-27 10:21:44 -0400

    Bug 1496396 - Don't warn on navigate when YAML isn't changed
    Don't prompt the user about unsaved changes on the edit YAML page if the
    user hasn't made any edits.
    
    Fixes https://bugzilla.redhat.com/show_bug.cgi?id=1496396

--- dist/scripts/templates.js
+++ dist/scripts/templates.js
@@ -9926,7 +9926,6 @@ angular.module('openshiftConsoleTemplates', []).run(['$templateCache', function(
     "      mode: 'yaml',\n" +
     "      theme: 'eclipse',\n" +
     "      onLoad: $ctrl.aceLoaded,\n" +
-    "      onChange: $ctrl.aceChanged,\n" +
     "      rendererOptions: {\n" +
     "        showPrintMargin: false\n" +
     "      }\n" +
