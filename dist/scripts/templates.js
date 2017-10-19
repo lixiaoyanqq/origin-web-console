46fd8707286d9966b53e9f64d4c06c1fccf976ed
Robb Hamilton <rhamilto@redhat.com>
2017-10-19 09:24:22 -0400

    Remove hero-icon and make empty-state-message more consistent
    Fixes #2288

--- dist/scripts/templates.js
+++ dist/scripts/templates.js
@@ -7791,6 +7791,13 @@ angular.module('openshiftConsoleTemplates', []).run(['$templateCache', function(
     "<div class=\"mar-top-lg mar-bottom-lg\">\n" +
     "<div class=\"separator\"></div>\n" +
     "</div>\n" +
+<<<<<<< HEAD
+=======
+    "<div ng-if=\"loading || !import\" class=\"empty-state-message text-center\">\n" +
+    "<h2 ng-if=\"!loading\" class=\"h2\">Select an image stream tag or enter an image name.</h2>\n" +
+    "<h2 ng-if=\"loading\" class=\"h2\">Loading image metadata for <span class=\"word-break\">{{imageName | stripSHA}}</span>...</h2>\n" +
+    "</div>\n" +
+>>>>>>> Remove hero-icon and make empty-state-message more consistent
     "<div class=\"row mar-bottom-md\" ng-if-start=\"!loading && import.image\">\n" +
     "<div class=\"col-sm-2 hidden-xs text-right h2\">\n" +
     "<span class=\"fa fa-cube text-muted\" style=\"font-size: 100px\" aria-hidden=\"true\"></span>\n" +
