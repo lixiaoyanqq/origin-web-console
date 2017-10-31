a9a6c24915debf3fe7dc13a9a1418aad68e294ca
Samuel Padgett <spadgett@redhat.com>
2017-10-31 16:17:31 -0400

    Show ProvisionCallFailed in notification drawer
    Also use `humanizeKind` filter when displaying kinds in the drawer.

--- app/scripts/constants.js
+++ app/scripts/constants.js
@@ -273,6 +273,7 @@ angular.extend(window.OPENSHIFT_CONSTANTS, {
     Deprovisioning: true,
     ErrorCallingProvision: true,
     ErrorInjectingBindResult: true,
+    ProvisionCallFailed: true,
     Provisioning: true,
     ReferencesNonexistentServiceClass: true,
     ReferencesNonexistentServicePlan: true,
